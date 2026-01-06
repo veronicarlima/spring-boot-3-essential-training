2285d3950fbe6d9efd998cb86f51ff4f0a0a3560
veronica.ramires <veronica_ramires@live.com>
2026-01-05 23:05:30 -0500

    tomorrow ajust issue

--- room-web-app/src/main/java/com/frankmoley/lil/roomwebapp/web/controller/StaffController.java
+++ room-web-app/src/main/java/com/frankmoley/lil/roomwebapp/web/controller/StaffController.java
@@ -5,6 +5,7 @@ import com.frankmoley.lil.roomwebapp.data.repository.EmployeesRepository;
 import com.frankmoley.lil.roomwebapp.web.model.Staff;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,7 @@ public class StaffController {
         this.employeesRepository = employeesRepository;
     }
 
+    @GetMapping
     public String getStaffPage(Model model){
         List<EmployeesEntity> employeesEntityList = this.employeesRepository.findAll();
         List<Staff> staff = new ArrayList<>(employeesEntityList.size());
